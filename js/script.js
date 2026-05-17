d72b96dfcd2628f6b603385a0567ef46c87abc90
Nikitkas <nikatinka8889@gmail.com>
2026-05-18 03:16:13 +0500

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const schemeSvg = document.querySelector('.scheme-svg')
 let cost = 800;
 let totalPrice = 0
+const menuBtn = document.querySelector('.m-menu')
+const menu = document.querySelector('.menu')
 const totalPriceTag = document.querySelector('.price-total')
 schemeSvg.addEventListener('click', (e)=>{
     if (e.target.classList.contains('booked')) {
@@ -13,3 +15,8 @@ schemeSvg.addEventListener('click', (e)=>{
     totalPriceTag.textContent = totalPrice
 })
 
+menuBtn.addEventListener('click', ()=>{
+    console.log('click')
+    menu.classList.toggle('is-open')
+
+})
